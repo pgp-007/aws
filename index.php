533cdcbf2e2916a72f8699a60c1584fee0f33b06
pgp <45320076+pgp-007@users.noreply.github.com>
2026-09-05 12:13:07 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Terramino deployed by PGP</title>
+  <title>EC2 Terramino deployed by PGP</title>
   <link rel="icon" href="https://www.terraform.io/favicon.ico" type="image/x-icon" />
   <style>
     html,
